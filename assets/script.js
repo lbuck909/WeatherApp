243226e4ff6daa8de49b7508ea564446f0563187
LaiSha Buck <laishabuck@gmail.com>
2023-04-01 22:15:35 -0400

    :style updates

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var searchbtnEl = document.querySelector('#citySubmit');
+var cityInputEl = document.querySelector('#cityInput')
+var todayWeatherEl = document.querySelector('#todayWeather')
+var weatherCardEl = document.querySelector('#weatherCard')
+var historyEl = document.querySelector('#history')
