a1251d8fe8216092528e798cf5360090c07148d9
LaiSha Buck <laishabuck@gmail.com>
2023-04-05 21:21:07 -0400

    added on click function for city search

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,58 @@
 var APIkey = "15cadfd0ee2b826d3df22a7c7961b0ee";
 var APIUrl = "https://api.openweathermap.org";
 
-//
+//Global Variables//
 var searchbtnEl = document.querySelector('#citySubmit');
 var cityInputEl = document.querySelector('#cityInput')
 var currentWeatherEl = document.querySelector('#currentWeather')
 var weatherCardEl = document.querySelector('#weatherCard')
 var historyEl = document.querySelector('#history')
+var currentTempEl = document.querySelector('#currentTemp')
 
-var currentWeatherEl = $('#todaysWeather');
-    currentWeatherEl.addClass('border border-primary');
+var CityConditions = [];
+// Inital data when user loads page
+//function startPage() {
+  //initalCityData();
+//}
 
-//use Open Weather Map API to get searched city weather//
-function getCurrentCityWeather(data) {
+// Uses localStorage to store persistent data//
 
-  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + APIkey;
-  fetch(weatherUrl)
-    .then(function(response) {
-      return response.json();
-    })
+var data = JSON.parse(localStorage.getItem('cityInput')) || [];
+//console.log('cityInput', data);
+// Create if & then conditional statement to weather API data//
+var APIUrl; 
+  if (location.protocol === 'https:') {
+    APIUrl = "https://api.openweathermap.org";
+  } else {
+    APIUrl = '"https://api.openweathermap.org";'
+  }
+// from criteria:  search for a city and receive current & future weather conditions with a search history shown
+function startPage(){
+  resetElement(weatherCardEl);
+//random list of city searches
+  if(cityData){
+var cityListElement = document.createElement('cityList');
+cityListElement.classList.add("random-list");
+cityListElement.classList.add('w-100');
+  
+
+//loop needed for local storage data
+for( var i =0; i < initalCityData.length; i++){
+  var listElement = document.createElement ('li');
+  var city = initalCityData[i];
+  var buttonHtml = `<button class="search-btn" attr="${city}">${city}</button>`;
+  listElement.innerHTML = buttonHtml;
+  var cityList = document.getElementById('city-list');
+  cityList.appendChild(listElement);
 }
+
+weatherCardEl.appendChild(listElement);
+}
+};
+//when the user clicks btn current city; use Jquery here
+$(document).on('click', ".list-weatherCardEL-item", function(event) {
+  event.preventDefault();
+ 
+  var city = $(this).attr('attr');
+  returnApiFetch(city);
+});
